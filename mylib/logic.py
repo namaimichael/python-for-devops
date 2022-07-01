831add35312e49ce317dc1d3dafed0fd557dc81a
Michael Namai <namaimichael@gmail.com>
2022-07-02 00:03:22 +0300

    Updated cli-fire function

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,7 +1,13 @@
+from unittest import result
 import wikipedia
 
 
 def wiki(name="War Goddess", length=1):
-    """This is wikipedia fetcher"""
+    """This is Wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    """Search Wikipedia for Names"""
+    result = wikipedia.search(name)
+    return result
